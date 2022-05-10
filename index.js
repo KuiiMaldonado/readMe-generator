3217a0dfb1cf59124db6a500596026454f7bc30d
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-05-10 16:27:03 -0500

    Defining the first questions

--- index.js
+++ index.js
@@ -2,13 +2,48 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Briefly describe your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Any installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How to use your application/project?',
+    },
+    {
+        type: 'input',
+        name: 'constributions',
+        message: 'What are the guidelines to contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Does your project have any tests?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(data => {
+        console.log(data);
+    });
+}
 
 // Function call to initialize app
 init();
